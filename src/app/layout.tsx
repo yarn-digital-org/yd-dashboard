93e290fedc111afc38ffa3ba80ff4b9a6de705e7
Yarn Digital <hello@yarndigital.co.uk>
2026-03-17 06:22:59 +0000

    Add Meta Pixel (2078268825978104) to root layout + update CSP
    - Added Facebook pixel base code with PageView tracking to root layout
    - Updated CSP to allow connect.facebook.net, www.facebook.com, and googletagmanager
    - Pixel fires on all pages including /free-audit landing page
    - Fixes BOFU ads showing 0 landing page views despite clicks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,24 @@ export default function RootLayout({
             } catch(e) {}
           })();
         `}} />
+        {/* Meta Pixel - Yarn Digital */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '2078268825978104');
+          fbq('track', 'PageView');
+        `}} />
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2078268825978104&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body className={inter.className} style={{ margin: 0, padding: 0, minHeight: '100vh' }}>
         <GoogleAnalytics measurementId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || 'G-ZEFG1FNZ2T'} />
